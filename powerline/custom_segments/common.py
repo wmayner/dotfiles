c985f963d69e0de7be445d97ce2a7befccb2056a
Will Mayner <wmayner@gmail.com>
2014-04-03 17:49:47 -0500

    Tweak full_path segment

--- powerline/custom_segments/common.py
+++ powerline/custom_segments/common.py
@@ -1,5 +1,8 @@
-# vim:fileencoding=utf-8:noet
+# vim:fileencoding=utf-8
 
+from __future__ import unicode_literals, absolute_import
+
+import os
 import re
 
 from powerline.lib.unicode import u
@@ -7,7 +10,7 @@ from powerline.theme import requires_segment_info
 
 
 @requires_segment_info
-def full_path(pl, segment_info):
+def full_path(pl, segment_info, dir_limit_depth=None, dir_root_length=3, ellipsis=" … "):
     """Returns a segment containing the full path of the current working
     directory."""
     try:
@@ -24,4 +27,21 @@ def full_path(pl, segment_info):
     if home:
         home = u(home)
         cwd = re.sub('^' + re.escape(home), '~', cwd, 1)
-    return cwd
+
+    # Split and cut out the current directory
+    cwd_split = cwd.split(os.sep)[:-1]
+
+    # Limit depth but include a bit of the beginning of the path
+    if dir_limit_depth and len(cwd_split) > dir_limit_depth:
+
+        if dir_root_length <= dir_limit_depth/2:
+            dir_limit_depth -= dir_root_length
+        else:
+            dir_root_length = dir_limit_depth/2
+            dir_limit_depth /= 2
+
+        first = cwd_split[:dir_root_length]
+        second = cwd_split[-dir_limit_depth:]
+        cwd_split = first + [ellipsis] + second
+
+    return os.sep.join(cwd_split)
