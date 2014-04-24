3148f50643e6955ce5176ff29faed8c334fc0bd7
Will Mayner <wmayner@gmail.com>
2014-04-24 17:46:45 -0500

    Move hooks to zsh, add solarized dircolors, add ~/bin

--- zsh/hooks.zsh
+++
@@ -1,9 +0,0 @@
-# get aliases
-source $ALIASES
-
-# Use .localrc for SUPER SECRET CRAP that you don't
-# want in your public, versioned repo.
-if [[ -a ~/.zshrc.local ]]
-then
-  source ~/.zshrc.local
-fi
