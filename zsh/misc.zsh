46e5685511fa63d36c40fb7b3459fa6158f0bd92
Will Mayner <wmayner@gmail.com>
2014-04-24 14:12:00 -0500

    Move misc to zshrc

--- zsh/misc.zsh
+++
@@ -1,5 +0,0 @@
-# Press Ctrl+Q to push the current command
-# on to the stack. It will disappear, allowing
-# you to enter another command, after which
-# it will reappear in the prompt.
-bindkey "^Q" push-input
