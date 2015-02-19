f0804c2820b5ca9ec3c13f7965df835521a92821
Will Mayner <wmayner@gmail.com>
2015-02-19 13:36:10 -0600

    Remove gpip commands

--- python/pip.zsh
+++ python/pip.zsh
@@ -1,19 +1,2 @@
 # use this instead of 'pip install' to keep things local and not require root
 alias pinstall="pip install --user"
-
-# use `globalpip` to use global pip
-gpip() {
-  PIP_REQUIRE_VIRTUALENV="" pip "$@"
-}
-# use `globalpip3` to use global pip3
-gpip3() {
-  PIP_REQUIRE_VIRTUALENV="" pip3 "$@"
-}
-# use `globalpinstall` to install global python 2 packages
-gpinstall() {
-  PIP_REQUIRE_VIRTUALENV="" pip install --user "$@"
-}
-# use `globalpinstall3` to install global python 3 packages
-gpinstall3() {
-  PIP_REQUIRE_VIRTUALENV="" pip3 install --user "$@"
-}
