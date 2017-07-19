fdc0c0906c394a5358b54b7f9bc22b14baed111b
Will Mayner <wmayner@gmail.com>
2017-07-18 22:55:53 -0500

    Fix PATH and virtualenv for new brewed Python

--- python/virtualenvwrapper.zsh
+++ python/virtualenvwrapper.zsh
@@ -1,5 +1,4 @@
 # virtualenvwrapper configuation:
-export VIRTUALENVWRAPPER_PYTHON="/usr/local/bin/python"
 export WORKON_HOME="$HOME/.virtualenvs"
 export PROJECT_HOME=$CODE
 source "/usr/local/bin/virtualenvwrapper.sh"
