2e712cdd790fc8cc63b4f1b3487eed285272e51c
Will Mayner <wmayner@gmail.com>
2017-07-20 14:50:32 -0500

    Add NEST stuff to PATH

--- python/config.zsh
+++ python/config.zsh
@@ -1,2 +1,3 @@
 # export PYTHONSTARTUP=$HOME/.pythonstartup.py
-export PYTHONPATH=$PYTHONPATH:~/.config/powerline/
+# export PYTHONPATH=$PYTHONPATH:~/.config/powerline/
+export PYTHONPATH="$HOME/nest-2.12.0/lib/python3.6/site-packages:$PYTHONPATH"
