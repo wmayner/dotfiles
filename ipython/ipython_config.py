96d4da39e8b7198b60d46ae3c59541e62f1f6df5
Will Mayner <wmayner@gmail.com>
2017-07-20 14:50:51 -0500

    Only print last expression in IPython

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -175,7 +175,7 @@ c.InteractiveShellApp.extensions = [
 
 # 'all', 'last', 'last_expr' or 'none', specifying which nodes should be run
 # interactively (displaying output from expressions).
-c.InteractiveShell.ast_node_interactivity = 'all'
+c.InteractiveShell.ast_node_interactivity = 'last_expr'
 
 # A list of ast.NodeTransformer subclass instances, which will be applied to
 # user input before code is run.
