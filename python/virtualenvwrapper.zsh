e30c42606086a8b2d0eb7c1764648c518af3a4a2
Will Mayner <wmayner@gmail.com>
2017-08-02 17:28:38 -0500

    Updates for CHTC

--- python/virtualenvwrapper.zsh
+++ python/virtualenvwrapper.zsh
@@ -1,5 +1,5 @@
 # virtualenvwrapper configuation:
-export VIRTUALENVWRAPPER_PYTHON="/usr/bin/python"
+export VIRTUALENVWRAPPER_PYTHON="/home/mayner/bin/python"
 export WORKON_HOME="$HOME/.virtualenvs"
 export PROJECT_HOME=$CODE
 export VIRTUAL_ENV_DISABLE_PROMPT=1
