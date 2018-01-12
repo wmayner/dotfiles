38d33f01a8a54dbed7aa971e88aa31d5a50a3613
Will Mayner <wmayner@gmail.com>
2018-01-12 17:06:18 -0600

    Finish install script

--- install.sh
+++ install.sh
@@ -1,18 +1,28 @@
-#!/bin/sh
+#!/usr/bin/env sh
+#
+# - Install homebrew if on macOS and brews various formulae
+# - Install oh-my-zsh
+# - Symlink all *.symlink files into $HOME as dotfiles
 
-echo "Installing oh-my-zsh...\n"
+# Are we on macOS or Linux?
+OS=$(uname -s)
 
-# Install oh-my-zsh
+if [ "$OS" == "Darwin" ]; then
+  echo "Installing Homebrew..."
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  BREW_FORUMLAE='./brew/formulae.txt'
+  echo "Installing brew formulae from '$BREW_FORUMLAE'..."
+  xargs brew install < $BREW_FORUMLAE
+fi
+
+echo "\nInstalling oh-my-zsh...\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo ''
 
-echo "\nDone!"
-
-# Symlink all `*.symlink` files into $HOME as dotfiles
-echo "\nSymlinking *.symlink files...\n"
-for SOURCE_FILE in $DOTFILES/**/*.symlink;
-do
+# Symlink dotfiles
+for SOURCE_FILE in $(find $(pwd) -name '*.symlink'); do
   LINK_FILE="$HOME/.$(basename ${SOURCE_FILE%.symlink})"
-  echo "$LINK_FILE -> $SOURCE_FILE";
+  echo "Symlinking $LINK_FILE -> $SOURCE_FILE";
   ln -s "$SOURCE_FILE" $LINK_FILE;
 done
 
