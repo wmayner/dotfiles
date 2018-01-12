c9630e4b4ff90608b0429b0bdcb3ad688dcdcfa8
Will Mayner <wmayner@gmail.com>
2018-01-12 17:06:34 -0600

    Add uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+#
+# Remove any dotfiles that are symlinks to '*.symlink' files in this
+# directory.
+
+for SOURCE_FILE in $(find $(pwd) -name '*.symlink'); do
+  LINK_FILE="$HOME/.$(basename ${SOURCE_FILE%.symlink})"
+  if [ "$(readlink $LINK_FILE)" == "$SOURCE_FILE" ]; then
+    echo "Removing symlink $LINK_FILE -> $SOURCE_FILE"
+    rm $LINK_FILE
+  fi
+  echo ''
+done
