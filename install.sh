d40fb134d4daf9276c0de54bd744cdb33affc0f0
Will Mayner <wmayner@gmail.com>
2018-01-15 13:11:15 -0600

    install.sh: Use `printf`

--- install.sh
+++ install.sh
@@ -19,34 +19,33 @@ OS=$(uname -s)
 
 # macOS-specific
 if [ "$OS" = "Darwin" ]; then
-  echo "\nInstalling Homebrew...\n"
+  printf "\nInstalling Homebrew...\n"
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   BREW_FORMULAE='./brew/formulae.txt'
   BREW_FORMULAE_HEAD='./brew/formulae-head.txt'
-  echo "Installing Homebrew formulae from '$BREW_FORMULAE' and '$BREW_FORMULAE_HEAD'..."
+  printf "Installing Homebrew formulae from '$BREW_FORMULAE' and '$BREW_FORMULAE_HEAD'..."
   xargs brew install < $BREW_FORMULAE
   xargs brew install --HEAD < $BREW_FORMULAE_HEAD
   # Ensure brewed Python is used instead of the system Python
   if [ ! -e "/usr/local/bin/python" ]; then
     ln -s "/usr/local/bin/python2" "/usr/local/bin/python"
   fi
-  echo ''
 fi
 
-echo "\nChanging shell to zsh...\n"
+printf "\nChanging shell to zsh...\n"
 # NOTE: You may have to run the following:
-#   sudo echo $(which zsh) >> /etc/shells`
+#   sudo printf $(which zsh) >> /etc/shells`
 chsh -s $(which zsh)
 
-echo "\nInstalling oh-my-zsh...\n"
+printf "\nInstalling oh-my-zsh...\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "\nInstalling Python packages...\n"
+printf "\nInstalling Python packages...\n"
 PYTHON_REQUIREMENTS_FILE='./python/requirements.txt'
 pip2 install --user --upgrade -r $PYTHON_REQUIREMENTS_FILE
 pip3 install --user --upgrade -r $PYTHON_REQUIREMENTS_FILE
 
-echo "\nMaking Neovim virtualenvs...\n"
+printf "\nMaking Neovim virtualenvs...\n"
 NEOVIM_REQUIREMENTS="./vim/neovim_requirements.txt"
 source $(which virtualenvwrapper.sh)
 # Python 2
@@ -60,21 +59,21 @@ source "$HOME/.virtualenvs/neovim-python3/bin/activate"
 pip install --upgrade -r $NEOVIM_REQUIREMENTS
 deactivate
 
-echo "\nInstalling Node packages...\n"
+printf "\nInstalling Node packages...\n"
 NODE_PACKAGES_FILE="./node/global_packages.txt"
 xargs npm install --global < $NODE_PACKAGES_FILE
 
-echo "\nSymlinking '*.symlink' files...\n"
+printf "\nSymlinking '*.symlink' files...\n"
 for SOURCE_FILE in $(find $(pwd) -name '*.symlink'); do
   LINK_FILE="$HOME/.$(basename ${SOURCE_FILE%.symlink})"
   ln -sv "$SOURCE_FILE" $LINK_FILE;
 done
 
-echo "\nSymlinking IPython profile...\n"
+printf "\nSymlinking IPython profile...\n"
 mkdir -p "$HOME/.ipython/profile_default"
 ln -sv "$(pwd)/ipython/ipython_config.py" "$HOME/.ipython/profile_default/ipython_config.py"
 
-echo "\nSetting up Vim and Neovim...\n"
+printf "\nSetting up Vim and Neovim...\n"
 # Vim
 mkdir -p "$HOME/.vim/autoload"
 # Install vim-plug
@@ -87,4 +86,4 @@ ln -sv "$HOME/.vim" "$HOME/.config/nvim"
 # Install plugins
 vim +PlugInstall! +qall
 
-echo "\nDone!"
+printf "\nDone!"
