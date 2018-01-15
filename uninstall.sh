15ecc4bb4b931aedf16c6767b7a454d3157856ec
Will Mayner <wmayner@gmail.com>
2018-01-15 14:08:58 -0600

    uninstall.sh: Reorder steps

--- uninstall.sh
+++ uninstall.sh
@@ -10,11 +10,6 @@ for SOURCE_FILE in $(find $(pwd) -name '*.symlink'); do
   fi
 done
 
-# Remove Neovim virtualenvs
-source $(which virtualenvwrapper.sh)
-rmvirtualenv neovim-python2
-rmvirtualenv neovim-python3
-
 # Remove IPython config
 LINK_FILE="$HOME/.ipython/profile_default/ipython_config.py"
 SOURCE_FILE="$(pwd)/ipython/ipython_config.py"
@@ -36,3 +31,8 @@ if [ "$(readlink $LINK_FILE)" == "$SOURCE_FILE" ]; then
   echo "Removing symlink $LINK_FILE -> $SOURCE_FILE"
   rm $LINK_FILE
 fi
+
+# Remove Neovim virtualenvs
+source $(which virtualenvwrapper.sh)
+rmvirtualenv neovim-python2
+rmvirtualenv neovim-python3
