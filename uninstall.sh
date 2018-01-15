8f4a8bd147bfc0ba6906c97ba0e424d5ea2688a9
Will Mayner <wmayner@gmail.com>
2018-01-15 12:56:45 -0600

    uninstall.sh: Special-case IPython symlink

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
-#
-# Remove any dotfiles that are symlinks to '*.symlink' files in this
-# directory.
 
+# Remove any dotfiles that are symlinks to '*.symlink' files in this
+# directory
 for SOURCE_FILE in $(find $(pwd) -name '*.symlink'); do
   LINK_FILE="$HOME/.$(basename ${SOURCE_FILE%.symlink})"
   if [ "$(readlink $LINK_FILE)" == "$SOURCE_FILE" ]; then
@@ -10,3 +9,11 @@ for SOURCE_FILE in $(find $(pwd) -name '*.symlink'); do
     rm $LINK_FILE
   fi
 done
+
+# Remove IPython config
+LINK_FILE="$HOME/.ipython/profile_default/ipython_config.py"
+SOURCE_FILE="$(pwd)/ipython/ipython_config.py"
+if [ "$(readlink $LINK_FILE)" == "$SOURCE_FILE" ]; then
+  echo "Removing symlink $LINK_FILE -> $SOURCE_FILE"
+  rm $LINK_FILE
+fi
