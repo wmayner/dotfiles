711f315ceddec3d5ac63837431d190025af996a3
Will Mayner <wmayner@gmail.com>
2018-01-15 13:29:47 -0600

    uninstall.sh: Remove Neovim config

--- uninstall.sh
+++ uninstall.sh
@@ -10,6 +10,21 @@ for SOURCE_FILE in $(find $(pwd) -name '*.symlink'); do
   fi
 done
 
+# Remove Neovim config
+mkdir -p "$HOME/.config/nvim/autoload"
+LINK_FILE="$HOME/.config/nvim/init.vim"
+SOURCE_FILE=$(find . -name "vimrc.symlink")
+if [ "$(readlink $LINK_FILE)" == "$SOURCE_FILE" ]; then
+  echo "Removing symlink $LINK_FILE -> $SOURCE_FILE"
+  rm $LINK_FILE
+fi
+LINK_FILE="$HOME/.vim"
+SOURCE_FILE="$HOME/.config/nvim"
+if [ "$(readlink $LINK_FILE)" == "$SOURCE_FILE" ]; then
+  echo "Removing symlink $LINK_FILE -> $SOURCE_FILE"
+  rm $LINK_FILE
+fi
+
 # Remove IPython config
 LINK_FILE="$HOME/.ipython/profile_default/ipython_config.py"
 SOURCE_FILE="$(pwd)/ipython/ipython_config.py"
