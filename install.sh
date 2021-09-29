a92f9a89f67c59a87a7625c9214495343b897cc4
Will Mayner <wmayner@gmail.com>
2021-09-29 14:13:46 -0500

    install.sh: Update

--- install.sh
+++ install.sh
@@ -2,20 +2,11 @@
 #
 # This script will
 # - Download and install Homebrew on macOS, and brew various formulae
-# - Install system packages and Neovim on Linux
+# - Install system packages on Linux
 # - Change default shell to zsh
 # - Download and install Oh My Zsh
-# - Install various Python packages
-# - Make virtualenvironments `neovim-python2` and `neovim-python3` and run
-#   `pip install neovim` in each
-# - Install global node packages
 # - Symlink all `*.symlink` files into $HOME as dotfiles
-# - Symlink IPython profile
-# - Download and install vim-plug
-# - Symlink Neovim configuration directory to Vim configuration directory
-# - Install Vim plugins
-
-NEOVIM_INSTALL_PREFIX="$HOME/neovim"
+# - Download & install vim-plug and then install plugins
 
 # Are we on macOS or Linux?
 OS=$(uname -s)
@@ -42,16 +33,6 @@ else
   printf "\nInstalling system packages...\n"
   LINUX_PACKAGES='./linux/packages.txt'
   xargs sudo apt-get install <$LINUX_PACKAGES
-  # Neovim
-  command -v nvim >/dev/null || {
-    printf "\nInstalling Neovim...\n" &&
-    git clone https://github.com/neovim/neovim.git "$HOME/neovim-src" &&
-    cd "$HOME/neovim-src" &&
-    make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$NEOVIM_INSTALL_PREFIX" &&
-    make install &&
-    cd "$DOTFILES" &&
-    rm -rf "$HOME/neovim-src"
-  }
 fi
 
 printf "\nChanging shell to zsh...\n"
@@ -62,50 +43,17 @@ chsh -s $(which zsh)
 printf "\nInstalling oh-my-zsh...\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-printf "\nInstalling Python packages...\n"
-PYTHON_REQUIREMENTS_FILE='./python/requirements.txt'
-pip2 install --user --upgrade -r $PYTHON_REQUIREMENTS_FILE
-pip3 install --user --upgrade -r $PYTHON_REQUIREMENTS_FILE
-
-printf "\nMaking Neovim virtualenvs...\n"
-NEOVIM_REQUIREMENTS="./vim/neovim_requirements.txt"
-source $(which virtualenvwrapper.sh)
-# Python 2
-mkvirtualenv -p $(which python2) neovim-python2 &&
-source "$HOME/.virtualenvs/neovim-python2/bin/activate"
-pip install --upgrade -r $NEOVIM_REQUIREMENTS
-deactivate
-# Python 3
-mkvirtualenv -p $(which python3) neovim-python3
-source "$HOME/.virtualenvs/neovim-python3/bin/activate"
-pip install --upgrade -r $NEOVIM_REQUIREMENTS
-deactivate
-
-printf "\nInstalling Node packages...\n"
-NODE_PACKAGES_FILE="./node/global_packages.txt"
-xargs npm install --global <$NODE_PACKAGES_FILE
-
 printf "\nSymlinking '*.symlink' files...\n"
 for SOURCE_FILE in $(find $(pwd) -name '*.symlink'); do
   LINK_FILE="$HOME/.$(basename ${SOURCE_FILE%.symlink})"
   ln -sv "$SOURCE_FILE" $LINK_FILE;
 done
 
-printf "\nSymlinking IPython profile...\n"
-mkdir -p "$HOME/.ipython/profile_default"
-ln -sv "$(pwd)/ipython/ipython_config.py" "$HOME/.ipython/profile_default/ipython_config.py"
-
-printf "\nSetting up Vim and Neovim...\n"
-# Neovim
-ln -sv "$HOME/.vim" "$HOME/.config/nvim"
-ln -sv $(find $(pwd) -name "vimrc.symlink") "$HOME/.config/nvim/init.vim"
+printf "\nSetting up Vim...\n"
 # Install vim-plug
 mkdir -p "$HOME/.vim/autoload"
 curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# Mapping overrides
-mkdir -p "$HOME/.vim/after/plugin"
-echo "call VimrcMappings()" >> "$HOME/.vim/after/plugin/overrides.vim"
 # Install plugins
 vim +PlugInstall! +qall
 
