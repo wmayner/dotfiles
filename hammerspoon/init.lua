29c191423d311429c5b27f91ea56fc766c23f9e5
Will Mayner <wmayner@gmail.com>
2023-02-27 16:23:52 -0600

    Update Hammerspoon config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,32 @@
+hs.loadSpoon("SpoonInstall")
+
+spoon.SpoonInstall.repos.ShiftIt = {
+    url = "https://github.com/peterklijn/hammerspoon-shiftit",
+    desc = "ShiftIt spoon repository",
+    branch = "master",
+ }
+
+spoon.SpoonInstall:andUse("ShiftIt", { repo = "ShiftIt" })
+-- Use Vim arrow keys
+spoon.ShiftIt:bindHotkeys({
+    maximum = { { 'ctrl', 'cmd' }, 'o' },
+    left = { { 'ctrl', 'cmd' }, 'k' },
+    down = { { 'ctrl', 'cmd' }, 'l' },
+    right = { { 'ctrl', 'cmd' }, ';' },
+    upleft = { { 'ctrl', 'cmd', 'shift'}, 'k' },
+    upright = { { 'ctrl', 'cmd', 'shift'}, ';' },
+    botleft = { { 'ctrl', 'cmd', 'alt'}, 'k' },
+    botright = { { 'ctrl', 'cmd', 'alt' }, 'l' },
+    up = { { 'ctrl', 'cmd', 'alt'}, 'o' },
+    center = { { 'ctrl', 'alt', 'cmd' }, 'c' },
+    nextScreen = { { 'alt', 'cmd' }, 'o' },
+    previousScreen = { { 'alt', 'cmd' }, 'l' },
+    toggleFullScreen = { { 'ctrl', 'alt', 'cmd' }, 'f' },
+    toggleZoom = { { 'ctrl', 'alt', 'cmd' }, 'z' },
+    resizeOut = { { 'ctrl', 'alt', 'cmd' }, '=' },
+    resizeIn = { { 'ctrl', 'alt', 'cmd' }, '-' }
+  })
+
 -- Disable animations
 hs.window.animationDuration = 0
 
@@ -10,68 +39,6 @@ hs.hotkey.bind({"cmd"}, "escape", function()
     hs.hints.windowHints()
 end)
 
--- Window manipulation with WinWin
-hs.loadSpoon('WinWin')
--- hints
-hs.hotkey.bind({"cmd"}, "escape", function()
-    hs.hints.windowHints()
-end)
--- undo
--- not working... seems to undo all previous moves?
--- hs.hotkey.bind({"ctrl", "shift", "cmd"}, "z", function()
---     spoon.WinWin.undo()
--- end)
--- full
-hs.hotkey.bind({"ctrl", "cmd"}, "o", function()
-    spoon.WinWin:moveAndResize('maximize')
-end)
--- halves
-hs.hotkey.bind({"ctrl", "cmd"}, "k", function()
-    spoon.WinWin:moveAndResize('halfleft')
-end)
-hs.hotkey.bind({"ctrl", "cmd"}, ";", function()
-    spoon.WinWin:moveAndResize('halfright')
-end)
-hs.hotkey.bind({"ctrl", "cmd", "shift"}, "o", function()
-    spoon.WinWin:moveAndResize('halfup')
-end)
-hs.hotkey.bind({"ctrl", "cmd"}, "l", function()
-    spoon.WinWin:moveAndResize('halfdown')
-end)
--- corners
-hs.hotkey.bind({"ctrl", "cmd", "alt"}, "k", function()
-    spoon.WinWin:moveAndResize('cornerSW')
-end)
-hs.hotkey.bind({"ctrl", "cmd", "alt"}, ";", function()
-    spoon.WinWin:moveAndResize('cornerSE')
-end)
-hs.hotkey.bind({"shift", "cmd", "alt"}, "k", function()
-    spoon.WinWin:moveAndResize('cornerNW')
-end)
-hs.hotkey.bind({"shift", "cmd", "alt"}, ";", function()
-    spoon.WinWin:moveAndResize('cornerNE')
-end)
--- next screen
-hs.hotkey.bind({"cmd", "alt"}, "o", function()
-    spoon.WinWin:moveToScreen('next')
-    spoon.WinWin:moveAndResize('maximize')
-end)
-hs.hotkey.bind({"cmd", "alt"}, ";", function()
-    spoon.WinWin:moveToScreen('next')
-    spoon.WinWin:moveAndResize('halfright')
-end)
-hs.hotkey.bind({"cmd", "alt"}, "k", function()
-    spoon.WinWin:moveToScreen('next')
-    spoon.WinWin:moveAndResize('halfleft')
-end)
-hs.hotkey.bind({"cmd", "alt", "shift"}, "o", function()
-    spoon.WinWin:moveToScreen('next')
-    spoon.WinWin:moveAndResize('halfup')
-end)
-hs.hotkey.bind({"cmd", "alt"}, "l", function()
-    spoon.WinWin:moveToScreen('next')
-    spoon.WinWin:moveAndResize('halfdown')
-end)
 -- focus underneath
 hs.hotkey.bind({"ctrl", "cmd"}, "'", function()
     local fw=hs.window.focusedWindow()
